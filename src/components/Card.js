617e9ab04a244110949cff17f9c430483dea7521
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-09-11 19:33:50 -0700

    public asset img route

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,8 @@ import React from 'react';
 const card = ({ name, email, id }) => {
 
     return (
-        <div className='tc bg-light dib br3 pa1 ma2 grow bw2 shadow-4'>
-
+        <div className='tc bg-light dib br3 pa2 ma2 grow bw2 shadow-4'>
+            {/* {`${process.env.PUBLIC_URL}/assets/img/img.png`}  */}
             <img src={`https://robohash.org/${id}?200x200`} alt="Drink" />
             <div>
                 <h2>{name}</h2>
