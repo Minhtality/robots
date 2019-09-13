a733d6143284a9a370bf49ba0585ce3de7fa78ee
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-09-12 17:46:56 -0700

    added state and event handler to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import CardList from './components/CardList'
+import React, { Component } from 'react';
+import CardList from './components/CardList';
+import SearchBox from './components/SearchBox';
 import { robots } from './components/robots';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <CardList robotsProp={robots} />  {/* robotProps can be anything. and 'robots' is linked to the const declared in "robots.js" w/ the import */}
-    </div>
-  );
+class App extends Component {
+  state = {
+    robots: robots,
+    searchField: ''
+  }
+
+  //event handler 
+  searchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+  render() {
+    //
+    const filteredResult = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+    return (
+      <div className="tc">
+        <h1>Robo Friends Finder</h1>
+        <SearchBox searchChange={this.searchChange} />
+        <CardList robots={filteredResult} />  {/* robotProps can be anything. and 'robots' is linked to the const declared in "robots.js" w/ the import */}
+      </div>
+    );
+  }
 }
 
 export default App;
